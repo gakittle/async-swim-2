fa298fc73e314b726f2d40bcff627dfd2c2dc145
Greg Kittleson <greg.kittleson@gmail.com>
2019-06-15 14:00:01 -0700

    Add 404 and 200 routes for getting background img

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -4,7 +4,7 @@ const headers = require('./cors');
 const multipart = require('./multipartUtils');
 
 // Path for the background image ///////////////////////
-module.exports.backgroundImageFile = path.join('.', 'background.jpg');
+module.exports.backgroundImageFile = path.join('.', 'js', 'background.jpg');
 ////////////////////////////////////////////////////////
 
 let messageQueue = null;
@@ -15,17 +15,30 @@ module.exports.initialize = queue => {
 module.exports.router = (req, res, next = () => {}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200, headers);
-    res.end();
+  if (req.url === '/background.jpg') {
+    fs.readFile(module.exports.backgroundImageFile, (err, data) => {
+      if (err) {
+        res.writeHead(404, headers);
+      } else {
+        res.writeHead(200, headers);
+        res.write(data);
+      }
+
+      res.end();
+      next();
+    });
+  } else {
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200, headers);
+      res.end();
+    }
+
+    if (req.method === 'GET') {
+      res.writeHead(200, headers);
+
+      const direction = req.url.split('=')[1];
+      res.end(direction);
+    }
+    next(); // invoke next() at the end of a request to help with testing!
   }
-
-  if (req.method === 'GET') {
-    res.writeHead(200, headers);
-
-    const direction = req.url.split('=')[1];
-    res.end(direction);
-  }
-
-  next(); // invoke next() at the end of a request to help with testing!
 };
